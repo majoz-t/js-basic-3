e2ac6600ae068e36ebf6d27ff24f3e87382dc6d8
majoz-t <majozta@gmail.com>
2025-12-09 23:22:57 +0100

    letters1

---
+++ src/letters.js
@@ -0,0 +1,20 @@
+function one(text1, text2){
+    const total1= text1 + text2;
+    return total1;
+}
+
+function two(text1){
+    return text1.lenght;
+}
+
+function three(text1){
+    return text1.toUpperCase();
+}
+
+function four(text1){
+    return text1.toLowerCase();
+}
+
+function five(text1,index){
+    return text1.charAt(index);
+}
