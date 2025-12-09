3c3e273d002415df904d6eddba21dfe4d624baab
majoz-t <majozta@gmail.com>
2025-12-09 23:04:52 +0100

    numbers1

---
+++ src/numbers.js
@@ -0,0 +1,24 @@
+function uno(a,b){
+    const result1= a + b;
+    return result1;
+}
+
+function dos(a,b){
+    const result2= a-b;
+    return result2;
+}
+
+function tres(a,b){
+    const result3= a*b;
+    return result3;
+}
+
+function cuatro(a,b){
+    const result4= a/b;
+    return result4;
+}
+
+function cinco(b,e){
+    const result5= b**e;
+    return result5;
+}
