8eb9f5d96c56bb5429f9b00c4a2731891f3e9071
majoz-t <majozta@gmail.com>
2025-12-16 00:48:12 +0100

    Complete numbers

--- src/numbers.js
+++ src/numbers.js
@@ -1,24 +1,49 @@
-function uno(a,b){
-    const result1= a + b;
-    return result1;
+function sumaUno(a,b){
+    return a + b;
 }
+console.log(sumaUno(2,8));
 
 function dos(a,b){
-    const result2= a-b;
-    return result2;
+    return  a-b;
 }
+console.log(dos(22,8));
 
 function tres(a,b){
-    const result3= a*b;
-    return result3;
+    return  a*b;
 }
+console.log(tres(3,5));
 
 function cuatro(a,b){
-    const result4= a/b;
-    return result4;
+     return  a/b;
 }
+console.log(cuatro(10,5));
 
 function cinco(b,e){
-    const result5= b**e;
-    return result5;
+    return b**e;
 }
+console.log(cinco(2,3));
+
+function seis(a,b){
+    return a % b;
+}
+console.log(seis(16,5));
+
+function siete(a){
+    return Math.sqrt(a);
+}
+console.log(siete(16));
+
+function ocho(a){
+    return Math.abs(a);
+}
+console.log(ocho(-5));
+
+function nueve(a){
+    return Math.round(a);
+}
+console.log(nueve(1.64));
+
+function diez(){
+    return Math.random();
+}
+console.log(diez());
